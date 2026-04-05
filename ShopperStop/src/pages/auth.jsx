81ef444f09bebe27e2138ac3b2446e5cf67975cd
Parvesh Kumar Sharma <parveshsharma8910@gmail.com>
2026-04-05 16:31:06 +0530

    Authentication

--- ShopperStop/src/pages/auth.jsx
+++ ShopperStop/src/pages/auth.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import './auth.css';
-
+import { useForm } from 'react-hook-form'
 function Authentication() {
     const [mode, setMode] = useState("signup")
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    function onSubmit() {
+        // alert("User SignedUp");
+    }
+
     return (
         <>
             <div className="authPage">
@@ -10,14 +20,28 @@ function Authentication() {
                     <div className="auth_container">
                         <h1 className="page_title">{mode === "signup" ? "SignUp" : "Login"}</h1>
                     </div>
-                    <form className="auth_form">
+                    <form className="auth_form" onSubmit={handleSubmit(onSubmit)}>
                         <div className="form_group">
                             <label className="form_label" htmlFor="email">Email</label>
-                            <input type="email" className="form_input" id="email" />
+                            <input type="email" className="form_input" id="email" {...register("email", { required: "Email is required" })} />
+                            {errors.email && (<span className="form_error">{errors.email.message}</span>)}
                         </div>
                         <div className="form_group">
                             <label className="form_label" htmlFor="password">Password</label>
-                            <input type="password" className="form_input" id="password" />
+                            <input type="password" className="form_input" id="password" {...register("password", {
+                                required: "password is required",
+                                minLength: {
+                                    value: 6,
+                                    message: "Password should be minimum of 6 length",
+                                },
+                                maxLength: {
+                                    value: 12,
+                                    message: "Password should be maximum of 12 length",
+                                }
+
+                            })} />
+                            {errors.password && (<span className="form_error">{errors.password.message}</span>)}
+
                         </div>
 
                         <button className="submitForm" type="submit">{mode === "signup" ? "SignUp" : "Login"}</button>
